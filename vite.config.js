36e5f73ea73a2dbf21bfb2da9d801be1ee9a68be
Christopher Arthaud <christopher.arthaud@gmail.com>
2024-01-23 06:48:30 +0100

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
+  /*  build: {
     rollupOptions: {
       external: ["react", "react-router", "react-router-dom"],
       input: {
@@ -14,8 +14,9 @@ export default defineConfig({
       output: {
         globals: {
           react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
-  },
+  }, */
 });
